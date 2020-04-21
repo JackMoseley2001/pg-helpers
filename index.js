686175ef96d28ac184b5fc2f2e4e92943e14b33a
Jack Moseley <jackwmoseley@gmail.com>
2020-04-21 13:59:25 +0100

    :art:

--- index.js
+++ index.js
@@ -10,9 +10,18 @@ const path = require('path');
  * @property {Object} parameters
  */
 
+/**
+ * @typedef FormattedSQL
+ * @property {String} sql
+ * @property {Array} parameters
+ */
+
 class Database {
+  /**
+   * @type {pg.Pool}
+   */
   db;
-  #_sqlRoot;
+  _sqlRoot;
 
   /**
    * @param {pg.PoolConfig} options
@@ -23,11 +32,16 @@ class Database {
     this._sqlRoot = sqlRoot;
   }
 
+  async deinit() {
+    await this.db.end();
+  }
+
   /**
    * @param {String} filePath
    */
-  #_getSQLFile(filePath) {
+  _getSQLFile(filePath) {
     const fileName = filePath.replace(/\\./g, '/') + '.sql';
+
     const file = fs.readFileSync(path.join(this._sqlRoot, fileName), {
       encoding: 'utf8'
     });
@@ -41,7 +55,7 @@ class Database {
    * @param {String} find
    * @param {String} replaceVal
    */
-  #_findAndReplaceAll(str, find, replaceVal) {
+  _findAndReplaceAll(str, find, replaceVal) {
     const findKey = `:${find}`;
     const findRegex = new RegExp(findKey, 'g');
     if (str.includes(findKey)) {
@@ -54,7 +68,7 @@ class Database {
    * @param {String} sql
    * @param {Object} queries
    */
-  #_includeAdditionalQueries(sql, queries) {
+  _includeAdditionalQueries(sql, queries) {
     Object.entries(queries).forEach(([key, value]) => {
       sql = this._findAndReplaceAll(sql, key, value);
     });
@@ -65,7 +79,7 @@ class Database {
    * @param {String} sql
    * @param {Object} parameters
    */
-  #_includeParameters(sql, parameters) {
+  _includeParameters(sql, parameters) {
     let paramCount = 1;
     const values = [];
     Object.entries(parameters).forEach(([key, value]) => {
@@ -78,15 +92,19 @@ class Database {
 
   /**
    * @param {SQLFormatOptions} options
+   * @returns {FormattedSQL}
    */
   formatSQL(options) {
     let sql = this._getSQLFile(options.file);
-    let output;
+    let output = { sql };
     if (
       options.additionalQueries &&
       typeof options.additionalQueries === 'object'
     ) {
-      sql = this._includeAdditionalQueries(sql, options.additionalQueries);
+      output.sql = this._includeAdditionalQueries(
+        sql,
+        options.additionalQueries
+      );
     }
     if (options.parameters && typeof options.parameters === 'object') {
       output = this._includeParameters(sql, options.parameters);
@@ -111,6 +129,9 @@ class Database {
    * @param {Array} parameters
    */
   async queryDatabase(sql, parameters = []) {
+    console.log(sql);
+    console.log(parameters);
+
     const connection = await this.db.connect();
     const results = await connection.query(sql, parameters);
     connection.release();
